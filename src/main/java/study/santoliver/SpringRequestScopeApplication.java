d3db26cdf4392755c3d9959df41384c90776d0ec
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-21 15:52:01 -0300

    publishing the project

---
+++ src/main/java/study/santoliver/SpringRequestScopeApplication.java
@@ -0,0 +1,13 @@
+package study.santoliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRequestScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRequestScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/santoliver/SpringRequestScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package study.santoliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRequestScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
