c157721b7f65624d4f093368f2cbbc628e3b3be6
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-21 20:12:56 -0300

    injecting the LoginProcessor

--- src/main/java/study/santoliver/controllers/LoginController.java
+++ src/main/java/study/santoliver/controllers/LoginController.java
@@ -3,12 +3,36 @@ package study.santoliver.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import study.santoliver.model.LoginProcessor;
 
 @Controller
 public class LoginController {
 
+    private final LoginProcessor loginProcessor;
+
+    public LoginController(LoginProcessor loginProcessor) {
+        this.loginProcessor = loginProcessor;
+    }
+
     @GetMapping("/")
     public String loginGet() {
+        return "login.html";
+    }
+
+    @PostMapping("/")
+    public String loginPost(@RequestParam String username,
+                            @RequestParam String password, Model model) {
+        boolean loggedIn = loginProcessor.login();
+
+        if (loggedIn) {
+            model.addAttribute("message", "You are now logged in.");
+        } else {
+            model.addAttribute("message", "Login failed!.");
+        }
+
         return "login.html";
     }
 
